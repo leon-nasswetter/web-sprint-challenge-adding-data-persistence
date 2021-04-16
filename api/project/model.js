b54af1bee6c56a1bcbef66ee815449b6b17614e3
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-04-16 01:11:42 -0700

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,32 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig");
+
+function getProjects() {
+
+    return db("projects").then((projects) => {
+        return projects.map((project) => {
+          return {
+            ...project,
+            project_completed: project.project_completed === 0 ? false : true,
+          };
+        });
+      });
+}
+
+const getById = (project_id) => {
+    return db("projects").where({project_id}).first();
+  };
+
+async function addProject(project) {
+    const resourceId = await db("projects").insert(project)
+    const resProject = await getById(resourceId)
+    return{
+        ...resProject,
+        project_completed: project.project_completed ? true : false,
+    }
+}
+
+module.exports = {
+    getProjects,
+    addProject
+} 
--- api/project/router.js
+++ api/project/router.js
@@ -1,8 +1,26 @@
 // build your `/api/projects` router here
 const router = require("express").Router()
+const Projects = require("./model");
 
-router.use("/", (req, res) => {
-    res.json("working")
+router.get("/", (req, res) => {
+    Projects.getProjects()
+        .then((projects) => {
+            res.status(200).json(projects)
+        })
+        .catch((err) => {
+            res.status(500).json({ err: err.message })
+        })
 })
 
-module.exports = router
+router.post("/", (req, res) => {
+    const dataInput = req.body
+    Projects.addProject(dataInput)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch((err) => {
+        res.status(500).json({ err: err.message })
+    })
+})
+
+module.exports = router 

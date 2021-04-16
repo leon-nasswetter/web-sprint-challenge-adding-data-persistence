9df060a904c1455ee83570891c82ac8f73c5fb16
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-04-16 01:31:04 -0700

    mvp passing all

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,41 @@
 // build your `Task` model here
+const db = require("../../data/dbConfig");
+
+function getTasks() {
+  return db("tasks as t")
+    .join("projects", "t.project_id", "projects.project_id")
+    .select(
+      "project_description",
+      "project_name",
+      "task_completed",
+      "task_notes",
+      "task_description"
+    )
+    .then((tasks) => {
+      return tasks.map((task) => {
+        return {
+          ...task,
+          task_completed: task.task_completed === 0 ? false : true,
+        };
+      });
+    });
+}
+
+const getById = (task_id) => {
+    return db("tasks").where({task_id}).first();
+  };
+
+async function addTask(task) {
+    const resId = await db("tasks").insert(task)
+    const resTask = await getById(resId)
+    return{
+        ...resTask,
+        task_completed: task.task_completed ? true : false,
+    }
+}
+
+module.exports = {
+  getTasks,
+  addTask
+};
+
--- api/task/router.js
+++ api/task/router.js
@@ -1,8 +1,26 @@
 // build your `/api/tasks` router here
 const router = require("express").Router()
+const Tasks = require("./model");
 
-router.use("/", (req, res) => {
-    res.json("working")
+router.get("/", (req, res) => {
+    Tasks.getTasks()
+        .then((tasks) => {
+            res.status(200).json(tasks)
+        })
+        .catch((err) => {
+            res.status(500).json({ err: err.message })
+        })
 })
 
-module.exports = router
+router.post("/", (req, res) => {
+    const dataInput = req.body
+    Tasks.addTask(dataInput)
+    .then(task => {
+        res.status(201).json(task)
+    })
+    .catch((err) => {
+        res.status(500).json({ err: err.message })
+    })
+})
+
+module.exports = router 

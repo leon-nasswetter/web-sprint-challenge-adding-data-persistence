938a67e906cc2a311914544107ca4d52c57201e5
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-04-16 00:27:55 -0700

    initial, setup

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,8 @@
 // build your `/api/projects` router here
+const router = require("express").Router()
+
+router.use("/", (req, res) => {
+    res.json("working")
+})
+
+module.exports = router
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,8 @@
 // build your `/api/resources` router here
+const router = require("express").Router()
+
+router.use("/", (req, res) => {
+    res.json("working")
+})
+
+module.exports = router

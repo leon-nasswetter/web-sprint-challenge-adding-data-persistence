0b654990af578d325af5595940d72853a2d697f4
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-04-16 00:50:43 -0700

    wip, get resources works

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,22 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig")
+
+function getResources() {
+    return db("resources")
+}
+
+const getById = (resource_id) => {
+    return  db("resources")
+        .where("resource_id", resource_id)
+        .first()    
+}
+
+async function addResource(resource) {
+    const newResource = await db("resources").insert(resource) //returns id of created object
+    return getById(newResource)
+}
+
+module.exports = {
+    getResources,
+    addResource
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,8 +1,15 @@
 // build your `/api/resources` router here
 const router = require("express").Router()
+const Resource = require("./model")
 
 router.use("/", (req, res) => {
-    res.json("working")
+    Resource.getResources()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(err => {
+            res.status(500).json({ err: err.message })
+        })
 })
 
 module.exports = router
